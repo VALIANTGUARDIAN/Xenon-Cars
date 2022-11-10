1bea09b5b6fa9058a978811f39c35fd1cf9285ca
DIGVIJAY <singhdigvijay703@gmail.com>
2022-11-10 08:06:42 +0530

    ..

---
+++ backend/config.js
@@ -0,0 +1,10 @@
+const api_config = {
+    port: 3000,
+    conn_url: 'mongodb+srv://valiantGuardian:0000aabb@cluster0.8oj0f2w.mongodb.net/?retryWrites=true&w=majority',
+    stripe_sk: 'sk_test_51Ix4d6SFVvoENMxpwdsmBrJmle3P4B971C7VpYx1Yl8qUR2d1KWIvxb6VY2ggi5ceYZtQJasM4wSwT2RZFVF5Sjw00xUxC9MYE',
+    email: 'singhdigvijay703@gmail.com',
+    app_password: 'jvqxoedbaklnjvcp',
+}
+
+
+module.exports = api_config;
--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
+import { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Navigate, Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { AppProvider } from "./AppContext";
+import Authenticate from './components/Authenticate';
 import Navbar from './components/navbar/Navbar';
+import Header from "./components/Header";
 import Home from './components/home/Home';
 import ProductDescription from './components/product/ProductDescription';
+import Register from './components/register';
+import Logout from './components/logout';
+
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(JSON.parse(sessionStorage.getItem("user")))
   return (
     <div className="App">
       <Navbar/>
+      <AppProvider currentUser={currentUser}>
       <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Navigate to="/home"/>}/>
+        
+        <Route path='login' element={<Authenticate/>}/>
+        <Route path='signup' element={<Register/>}/>
         <Route path='home' element={<Home/>}/>
-        <Route path='product/:id' element={<ProductDescription/>}/>
+        <Route path='product/:id' element={<ProductDescription/>}/>        
+        <Route path='logout' element={<Logout/>}/>
       </Routes>        
       </BrowserRouter>
+      </AppProvider>
       
     </div>
   );
